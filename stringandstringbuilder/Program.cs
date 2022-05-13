b10ee451d692f95ee344add03d0d4f268212c703
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 09:08:28 -0500

    Add files via upload

---
+++ stringandstringbuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = " ";
+        Stopwatch ss = new Stopwatch();
+        ss.Start();
+        for (int i = 1; i <= 10000000; i++)
+        {
+            s = s + i;
+        }
+        ss.Stop();
+
+        StringBuilder sb = new StringBuilder();
+        Stopwatch ss1 = new Stopwatch();
+        ss1.Start();
+        for(int i=0; i <= 10000000; i++)
+        {
+            sb.Append(i);
+        }
+        ss1.Stop();
+        Console.WriteLine("time taken for string:" + ss.ElapsedMilliseconds);
+        Console.WriteLine("time taken for stringbuilder:" + ss1.ElapsedMilliseconds);
+        Console.ReadLine();
+    }
+}
+
